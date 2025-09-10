267731c64308a4eae60e7bb289a78d443908639a
George Simeonov <george.georgiev.simeonov@gmail.com>
2025-09-10 22:16:50 +0300

    jumps

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+const SPEED = 100.0
 const JUMP_POWER = -900.0
 const GRAVITY = 1000.0
 
-const FRICTION = 200.0
+const FRICTION = 1000.0
 const WALL_JUMP_PUSHBACK = 400.0
 const WALL_SLIDE_SPEED = 100.0
 
@@ -13,33 +13,45 @@ var is_wall_sliding = false
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
 	handle_movement(delta)
-	handle_jump()
-	handle_wall_slide(delta)
 
-	move_and_slide()
 
 func apply_gravity(delta: float) -> void:
 	if !is_on_floor() and !is_wall_sliding:
 		velocity.y += GRAVITY * delta
 
-func handle_movement(_delta: float) -> void:
+func handle_movement(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
+	var applied_horizontal_friction = 0
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * 4
+		applied_horizontal_friction = 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * _delta)
-
+		if(is_on_floor()):
+			velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+			applied_horizontal_friction = FRICTION
+	
+	
+	if(applied_horizontal_friction > 0):
+		print_debug("friction is being applied")
+	handle_jump()
+	move_and_slide()
+	
 func handle_jump() -> void:
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up"):
 		if is_on_floor():
 			velocity.y = JUMP_POWER
-	elif is_on_wall():
-		velocity.y = JUMP_POWER
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = WALL_JUMP_PUSHBACK
-	else:velocity.x = -WALL_JUMP_PUSHBACK
+		elif is_on_wall_only():
+			for i in range(get_slide_collision_count()):
+				var collision = get_slide_collision(i)
+				var normal = collision.get_normal()
+				if normal.x > 0:
+					#wall on left
+					velocity.x = WALL_JUMP_PUSHBACK
+					velocity.y = JUMP_POWER
+				elif normal.x < 0:
+					#wall on right
+					velocity.x = -WALL_JUMP_PUSHBACK
+					velocity.y = JUMP_POWER
 
-func handle_wall_slide(_delta: float) -> void:
-	is_wall_sliding = is_on_wall() and !is_on_floor() and velocity.y > 0
-	if is_wall_sliding:
-		velocity.y = min(velocity.y, WALL_SLIDE_SPEED)
+#func detect_wall_direction() -> int:
+	
